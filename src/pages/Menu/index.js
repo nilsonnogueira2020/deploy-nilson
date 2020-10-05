188134cdcb0a00690066743789053cab2cfc6479
Nilson Nogueira <nilson.nogueira@gmail.com>
2020-10-05 17:23:05 -0300

    commit login

--- src/pages/Menu/index.js
+++ src/pages/Menu/index.js
@@ -25,7 +25,7 @@ export default function Menu() {
             </form>
             </li>
             <li className="user">
-            <FaUserCircle size={30} color="white" />
+            <FaUserCircle size={30} color="white" /><p>olá, faça seu login ou cadastre-se</p>
             </li>
             <li className="user">
             <FaRegHeart size={30} color="white" />
