536819c6c11afb7dcfca63eb3c6de957521ae30c
Nilson Nogueira <nilson.nogueira@gmail.com>
2020-10-05 16:06:52 -0300

    commit menu

--- src/pages/Menu/index.js
+++ src/pages/Menu/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './styles.css';
+
 import { Link } from 'react-router-dom';
-import { FaLaptop } from 'react-icons/fa';
+import { FaUserCircle, FaSearch, FaRegHeart, FaShoppingCart, FaSearchLocation } from 'react-icons/fa';
+
 import logo from "../../assets/logo.jpg"
 
 export default function Menu() {
@@ -9,28 +11,63 @@ export default function Menu() {
         <section>
             
             <span>
-
+             <ul>
+                 <li>   
             <img className="logo"src={logo} alt="logo" />
+                </li>
+                <li>
+            <form>
+                   <ul>
+                    <li>
+                <input type="search" name="name" placeholder="busque aqui seu produto" /></li> 
+                <li><button type="submit">< FaSearch size={30} color="red"/></button></li>
+                
+                </ul>
+            </form>
+            </li>
+            <li className="user">
+            <FaUserCircle size={30} color="white" />
+            </li>
+            <li className="user">
+            <FaRegHeart size={30} color="white" />
+            </li>
+            <li className="cart">
+            <FaShoppingCart size={30} color="white" />
+            </li>
+            </ul>
+            </span>
+            <span>
+        
+            
 
             </span>
 
             <span>
 
                 <ul>
+                
                     <li>
-                        <Link className="style-link" to="/">Home</Link>
+                        <Link className="style-link" to="/"><FaSearchLocation size={20} color="yellow" />Informe seu Cep </Link>
                     </li>
 
                     <li>
-                        <Link className="style-link" to="/portfolio">Portfólio</Link>
+                        <Link className="style-link" to="/portfolio">seja a mais</Link>
                     </li>
                     <li>
-                        <Link className="style-link" to="/quemsomos">Quem Somos</Link>
+                        <Link className="style-link" to="/quemsomos">a empresas</Link>
                     </li>
+                    <li>
+                        <Link className="style-link" to="/quemsomos">vote na gente</Link>
+                    </li>
+                    <li>
+                        <Link className="style-link" to="/quemsomos">baixe o app</Link>
+                    </li>
+                   
                 </ul>
-                <FaLaptop size={50} />
+                
             </span>
         </section>
+        
 
     );
 }
--- src/pages/Topinho/index.js
+++ src/pages/Topinho/index.js
@@ -3,14 +3,14 @@ import topinho from "../../assets/topinho.jpg"
 import './styles.css';
 export default function Topinho() {
     return (
-        <section>
+        
             
             <span>
 
                 <img src={topinho} alt="topinho" />
 
             </span>
-        </section>
+        
             
           
     );
